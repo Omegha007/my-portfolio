832dbad0e3eb7dbd56afd8d58b08ea7183a2d6f5
Omegha007 <meghanathanmsd@gmail.com>
2024-01-25 20:35:09 +0530

    portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,259 @@
+export const Bio = {
+  name: "Meghanathan A",
+  roles: [
+    "Full Stack Developer",
+    "Frontend Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Omegha007",
+  resume:
+    "https://drive.google.com/file/d/1cyYBU9K3NF4DVXhzgf40OOcAZQicphyJ/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/meghanathan-a-329a29241/",
+  twitter: "https://twitter.com/Meghana21855967",
+  insta: "https://www.instagram.com/it.s_me_ghanathan/",
+  facebook: "https://www.facebook.com/profile.php?id=100090415068977",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      
+      
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+     
+      
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      
+     
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+     
+      {
+        name: "djangoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      
+      
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+     
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+    role: "Frontend developer Intern",
+    company: "Senchola University Pvt. Ltd.",
+    date: "Sep 2023 - Dec 2023",
+    desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+    skills: [
+      "HTML",
+      "CSS",
+      "Bootstraps",
+      "Javascript",
+      "React JS",
+      
+    ],
+    doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+ 
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://mce.edu.in/wp-content/uploads/2019/12/logo-150x150.png",
+    school: "Meenakshi Collage of Engineering ",
+    date: "Oct 2018 - May 2022",
+    grade: "8.1 CGPA",
+    desc: "I Completed my Bachelor's degree in Computer Science and Engineering at Meenakshi Collage of Engineering, Chennai. I have completed all semesters and have a CGPA of 8.1. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Engineering -  Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4Zr6j0LHmY8R0enE0xBxN-f4aH_PXMMu9QQ&usqp=CAU",
+    school: "MAK Matriculation Higher Secondary School",
+    date: "Apr 2016 - Apr 2018",
+    grade: "75%",
+    desc: "I completed my class 12 high school education at MAK Matriculation Higher Secondary  School, Chennai, where I studied Science with Computer Science.",
+    degree: "HSC(XII), Science with Computer",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4Zr6j0LHmY8R0enE0xBxN-f4aH_PXMMu9QQ&usqp=CAU",
+    school: "MAK Matriculation Higher Secondary School",
+    date: "Apr 2015 - Apr 2016",
+    grade: "75%",
+    desc: "I completed my class 10 education at MAK Matriculation Higher Secondary School,Chennai, where I studied Science with Computer Application.",
+    degree: "SSLC(X), Science with Computer",
+  },
+];
+
+export const projects = [
+  
+  
+  {
+    id: 1,
+    title: "API Feach",
+    date: "Oct 2023",
+    description:
+      "The Fetch API provides a JavaScript interface for accessing and manipulating parts of the protocol, such as requests and responses. It also provides a global fetch() method that provides an easy, logical way to fetch resources asynchronously across the network.",
+    image:
+      "https://rb.gy/5ycvca",
+    tags: [
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+      "Redux",
+      "NodeMailer",
+    ],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Project-Management-App",
+    webapp: "https://vexa-app.netlify.app/",
+  },
+ 
+  {
+    id: 3,
+    title: "CRUD APP",
+    date: "NOV 2023 ",
+    description:
+      "CRUD is an acronym that comes from the world of computer programming and refers to the four functions that are considered necessary to implement a persistent storage application: create, read, update and delete.",
+    image:
+      "https://rb.gy/s7kamb",
+    tags: ["Kotlin", "MVVM", "Room Database", "Google Drive Cloud API"],
+    category: "android app",
+    github: "https://github.com/rishavchanda/Buckoid-Android-App",
+    webapp: "https://play.google.com/store/apps/details?id=com.rishav.buckoid",
+  },
+  {
+    id: 10,
+    title: "Job Finding App",
+    date: "Oct 2023",
+    description:
+      "A Job Finding App made with React Native, Axios. Users can search for any job coming from API and apply there.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/255237090-cf798a2c-1b41-4bb7-b904-b5353a1f08e8.png",
+    tags: ["React Native", "JavaScript", "Axios"],
+    category: "android app",
+    github: "https://github.com/rishavchanda/Job-finder-App",
+    webapp: "https://github.com/rishavchanda/Job-finder-App",
+  },
+  {
+    id: 4,
+    title: "Instagram Clone",
+    date: "Dec 2023",
+    description:
+      "A Instagram clone made with React JS, Firebase, and Material UI. It has a chat room where users can chat with each other. It has a sidebar where users can see all the chat rooms and can create a new chat room. It has a login page where users can log in with their Google account.",
+    image:
+      "http://surl.li/ojmoh",
+    tags: ["React Js", "Firebase", "JavaScript"],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Whatsapp-Clone-React-Js",
+    webapp: "https://whatsapp-clone-rishav.web.app",
+  },
+  {
+    id: 5,
+    title: "Todo Web App",
+    date: "Sep 2023",
+    description:
+      " A Todo Web App made with React JS, Redux, and Material UI. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the tasks and can create a new task. It has a calendar where users can see all the tasks on a particular date. It has a search bar where users can search for a particular task.",
+    image:
+      "https://camo.githubusercontent.com/a328255ad96f861f57d25096d28018ab2656c689a1456b0d145764009bed2d1a/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f74253230283938292e706e673f616c743d6d6564696126746f6b656e3d33643335646366322d626666322d343730382d393031632d343232383866383332386633",
+    tags: ["React Js", "Local Storage", "AWS Auth", "Node JS"],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Todo-Web-App",
+    webapp: "https://rishav-react-todo.netlify.app/",
+  },
+  {
+    id: 6,
+    title: "Meta Landing Page",
+    date: "Oct 2023",
+    description:
+      "A simple react app that shows the characters of the famous Meta Product. It uses the Meta API to fetch the data. It also has a search bar to search for a particular product.",
+    image:
+      "https://camo.githubusercontent.com/cd07010cbeb90cb1b43a5d6162784326aef02210ef7d41a0f9ae043b3e392378/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f7425323028313534292e706e673f616c743d6d6564696126746f6b656e3d65613439383630632d303435362d343333342d616435372d336239346663303333363263",
+    tags: ["React Js", "API", "Axios", "Node JS"],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Breaking-Bad",
+    webapp: "https://breaking-bad-webapp.netlify.app",
+  },
+  
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
