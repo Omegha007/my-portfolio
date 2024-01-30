3f8c3429022bed556e0b2f6d3c57b93c81c862fa
Omegha007 <meghanathanmsd@gmail.com>
2024-01-30 23:21:04 +0530

    first commit

--- src/data/constants.js
+++ src/data/constants.js
@@ -18,15 +18,13 @@ export const Bio = {
 
 export const skills = [
   {
-    title: "Frontend",
+    title: "Front-End",
     skills: [
       {
         name: "React Js",
         image:
           "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
       },
-      
-      
       {
         name: "HTML",
         image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
@@ -46,63 +44,58 @@ export const skills = [
         image:
           "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
       },
-     
-      
+      {
+        name: "jQuery",
+        image:
+          "https://w7.pngwing.com/pngs/662/163/png-transparent-jquery-logo-web-development-jquery-ui-javascript-computer-icons-jqlogo-emblem-label-text-thumbnail.png",
+      },
     ],
   },
   {
-    title: "Backend",
+    title: "Back-End",
     skills: [
-      {
-        name: "Node Js",
-        image: "https://nodejs.org/static/images/logo.svg",
-      },
-      
-     
       {
         name: "Python",
         image:
           "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
       },
       {
+        name: "Django",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlFFyY16N5NRhzoG24RqB7x8Ok2t8Bdgs-tuMsOBXS2Q&s",
+      },
+      {
         name: "MySQL",
         image:
           "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
       },
-     
       {
-        name: "djangoDB",
+        name: "MongoDB",
         image:
           "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
       },
-      {
-        name: "Firebase",
-        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
-      },
     ],
   },
-  
+
+
   {
-    title: "Others",
+    title: "Tools",
     skills: [
       {
         name: "Git",
         image:
-          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
       },
       {
         name: "GitHub",
         image:
           "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
       },
-      
-      
       {
         name: "VS Code",
         image:
           "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
       },
-     
     ],
   },
 ];
@@ -110,22 +103,24 @@ export const skills = [
 export const experiences = [
   {
     id: 0,
-    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
-    role: "Frontend developer Intern",
-    company: "Senchola University Pvt. Ltd.",
-    date: "Sep 2023 - Dec 2023",
-    desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+    img: "https://shorturl.at/duyEO",
+    role: "Frontend Engineer Intern",
+    company: "Shenchola Technology Solutions",
+    date: "September 2023 - Pursuing",
+    desc: " I delved deep into the World of HTML, CSS, JavaScript, React Js, jQuery and FrameWorks like CSS BootStrap & React Js BootStrap to craft dynamic and visually stunning User Interfaces. Delivered 15+ Simple yet elegant Front-End projects on GitHub showcasing a diverse range of skills.",
     skills: [
+      "React JS",
+      "Redux",
+
+      "Material UI",
       "HTML",
       "CSS",
-      "Bootstraps",
-      "Javascript",
-      "React JS",
-      
+      "BootStrap",
+      "JavaScript"
     ],
-    doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+    doc: "",
   },
- 
+
 ];
 
 export const education = [
